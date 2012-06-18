5de1236284d779cbef8bdea36a0e49ed6e062786
chen guoqing <chen77716@gmail.com>
2012-06-18 13:41:18 +0800

    remove address limit

--- server/src/main/java/com/hopper/server/Endpoint.java
+++ server/src/main/java/com/hopper/server/Endpoint.java
@@ -4,60 +4,58 @@ import java.net.InetAddress;
 
 /**
  * {@link Endpoint} representing a communication node
- * 
+ *
  * @author chenguoqing
- * 
  */
 public class Endpoint {
-	/**
-	 * Id(configuration)
-	 */
-	public final int serverId;
+    /**
+     * Id(configuration)
+     */
+    public final int serverId;
 
-	/**
-	 * Address
-	 */
-	public final InetAddress address;
+    /**
+     * Address
+     */
+    public final InetAddress address;
 
-	/**
-	 * Port
-	 */
-	public final int port;
+    /**
+     * Port
+     */
+    public final int port;
 
-	/**
-	 * Constructor with all fields
-	 */
-	public Endpoint(int serverId, InetAddress address, int port) {
-		if (serverId < 0 || address == null || port < 0) {
-			throw new IllegalArgumentException();
-		}
+    /**
+     * Constructor with all fields
+     */
+    public Endpoint(int serverId, InetAddress address, int port) {
+        if (serverId < 0 || port < 0) {
+            throw new IllegalArgumentException();
+        }
 
-		this.serverId = serverId;
-		this.address = address;
-		this.port = port;
-	}
+        this.serverId = serverId;
+        this.address = address;
+        this.port = port;
+    }
 
-	@Override
-	public int hashCode() {
-		int result = 31 + address.hashCode();
-		result = 31 * result + port;
-		result = 31 * result + serverId;
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        int result = 31 + address.hashCode();
+        result = 31 * result + port;
+        result = 31 * result + serverId;
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
 
-		if (obj == null || getClass() != obj.getClass()) {
-			return false;
-		}
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
 
-		Endpoint other = (Endpoint) obj;
+        Endpoint other = (Endpoint) obj;
 
-		return serverId == other.serverId && address.equals(other.address)
-				&& port == other.port;
-	}
+        return serverId == other.serverId && address.equals(other.address) && port == other.port;
+    }
 }
